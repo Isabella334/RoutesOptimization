998dfba9f72987cf738fe936a816cd1a2ace0a06
Diego Flores <flo23714@uvg.edu.gt>
2026-05-15 00:25:13 -0600

    feat: add crossover module and tests

---
+++ backend/src/use_cases/genetic_algorithm/crossover.py
@@ -0,0 +1,27 @@
+import random
+from .route import Route
+
+class OrderedCrossover:
+    def crossover(self, parent1: Route, parent2: Route) -> Route:
+
+        size = len(parent1.route)
+
+        start, end = sorted(random.sample(range(size), 2))
+
+        child = [None] * size
+
+        child[start:end] = parent1.route[start:end]
+
+        parent2_places = [
+            place for place in parent2.route
+            if place not in child
+        ]
+
+        pointer = 0
+
+        for i in range(size):
+            if child[i] is None:
+                child[i] = parent2_places[pointer]
+                pointer += 1
+
+        return Route(child, parent1.closed)
